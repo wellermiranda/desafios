2e1ebb53b9dead6466bba88cc50b107c6edeba61
weller <weller.miranda@gmail.com>
2019-05-25 18:01:17 -0300

    fix strings tests

--- tests/strings/breaker.test.js
+++ tests/strings/breaker.test.js
@@ -1,12 +1,19 @@
+jest.mock('../../strings/justifier.js')
+
 const breaker = require('../../strings/breaker.js')
+const justifier = require('../../strings/justifier.js')
 
 describe('breaker', () => {
+
+    beforeEach(justifier.mockReset)
+
     it('should break lines with default characters by line (40)', () => {
         const text = 'In the beginning God created the heavens and the earth. Now the earth was formless and empty, darkness was over the surface of the deep, and the Spirit of God was hovering over the waters.\n\nAnd God said, "Let there be light," and there was light. God saw that the light was good, and he separated the light from the darkness. God called the light "day," and the darkness he called "night." And there was evening, and there was morning - the first day.'
         const expected = 'In the beginning God created the heavens\nand the earth. Now the earth was\nformless and empty, darkness was over\nthe surface of the deep, and the Spirit\nof God was hovering over the waters.\n\nAnd God said, "Let there be light," and\nthere was light. God saw that the light\nwas good, and he separated the light\nfrom the darkness. God called the light\n"day," and the darkness he called\n"night." And there was evening, and\nthere was morning - the first day.'
 
         const actual = breaker({text, justify: false})
 
+        expect(justifier).not.toBeCalled()
         expect(actual).toEqual(expected)
     })
 
@@ -17,15 +24,19 @@ describe('breaker', () => {
 
         const actual = breaker({text, charactersByLine, justify: false})
 
+        expect(justifier).not.toBeCalled()
         expect(actual).toEqual(expected)
     })
 
     it('should justify lines', () => {
-        const text = 'In the beginning God created the heavens and the earth. Now the earth was formless and empty, darkness was over the surface of the deep, and the Spirit of God was hovering over the waters.\n\nAnd God said, "Let there be light," and there was light. God saw that the light was good, and he separated the light from the darkness. God called the light "day," and the darkness he called "night." And there was evening, and there was morning - the first day.'
-        const expected = 'In the beginning God created the heavens\nand   the  earth.   Now  the  earth  was\nformless  and empty,  darkness was  over\nthe  surface of the deep, and the Spirit\nof  God was  hovering over  the  waters.\n\nAnd  God said, "Let there be light," and\nthere  was light. God saw that the light\nwas  good, and  he separated  the  light\nfrom  the darkness. God called the light\n"day,"   and  the   darkness  he  called\n"night."  And  there  was  evening,  and\nthere  was  morning  -  the  first  day.'
+        const line = 'and the earth. Now the earth was'
+        const expected = 'and   the  earth.   Now  the  earth  was'
+        const charactersByLine = 40
+        justifier.mockReturnValueOnce(expected)
 
-        const actual = breaker({text, justify: true})
+        const actual = breaker({text: line, justify: true, charactersByLine})
 
+        expect(justifier).toBeCalledWith({ charactersByLine, line })
         expect(actual).toEqual(expected)
     })
 })
