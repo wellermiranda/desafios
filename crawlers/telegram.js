8883d5cd5c7f5879ee981b3cadf331430293e290
weller <weller.miranda@gmail.com>
2019-05-26 16:09:01 -0300

    error handling

--- crawlers/telegram.js
+++ crawlers/telegram.js
@@ -10,12 +10,16 @@ const toHtml = ({threadLink, title, subreddit, upvotes, threadCommentsLink}) =>
     `<a href="${threadLink}">${title}</a>\nSubreddit: ${subreddit} | Votes: ${upvotes} | <a href="${threadCommentsLink}">Comments</a>\n`
 
 bot.command('NadaPraFazer', async ({replyWithHTML, update: { message: {text} }}) => {
-    let [,subreddits, punctuation] = text.split(' ')
-    punctuation = (punctuation && parseInt(punctuation)) || undefined
-    const threads = (await reader(subreddits, punctuation)).map(toHtml)
-    return threads && threads.length
-        ? threads.map(html => replyWithHTML(html))
-        : replyWithHTML(`Ops, não encontrei nada relevante sobre <strong>${subreddits}</strong>.`)
+    try {
+        let [, subreddits, punctuation] = text.split(' ')
+        punctuation = (punctuation && parseInt(punctuation)) || undefined
+        const threads = (await reader(subreddits, punctuation)).map(toHtml)
+        return threads && threads.length
+            ? threads.map(html => replyWithHTML(html))
+            : replyWithHTML(`Ops, não encontrei nada relevante sobre <strong>${subreddits}</strong>.`)
+    } catch(e) {
+        return replyWithHTML(`Ops, ocorreu um erro durante a busca sobre <strong>${subreddits}</strong>.`)
+    }
 })
 
 bot.launch()
