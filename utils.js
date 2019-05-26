fb35cb51551fc898638d29d948653f7b98b92ddd
weller <weller.miranda@gmail.com>
2019-05-26 01:55:33 -0300

    remove unused express

--- utils.js
+++ utils.js
@@ -4,10 +4,10 @@ const toInt = x => Number.isInteger(parseInt(x)) ? parseInt(x) : 0
 
 // It's just a workaround to keep heroku running the bot
 const herokuPortFix = () => {
-    const express = require('express')
-    const app = express()
+    const http = require('http')
     const port = process.env.PORT || 3000
-    app.listen(port, () => console.log(`Listening on port ${port}`))
+    const server = http.createServer((req, res) => res.end('hi'))
+    server.listen(port, '127.0.0.1', () => console.log(`Listening on port ${port}`))
 }
 
 module.exports = {compose, flat, toInt, herokuPortFix}
